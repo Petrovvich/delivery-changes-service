e794f09314329ec9272e0a8751da883349521131
Alexander Romaniuk <407@smsfinance.ru>
2019-04-29 21:44:13 +0700

    init dev branch, init module

---
+++ dcs/src/main/java/ru/petrovich/test/dcs/DcsApplication.java
@@ -0,0 +1,13 @@
+package ru.petrovich.test.dcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DcsApplication.class, args);
+    }
+
+}
