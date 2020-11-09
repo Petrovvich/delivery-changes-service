793f155130097a6003d22243928978897add3d18
Alexander Romaniuk <a.romanyuk@lantan.info>
2020-11-09 15:31:42 +0700

    test fixes

--- dcs/src/test/java/ru/petrovich/test/dcs/DcsApplicationTests.java
+++ dcs/src/test/java/ru/petrovich/test/dcs/DcsApplicationTests.java
@@ -1,23 +1,14 @@
 package ru.petrovich.test.dcs;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import ru.petrovich.test.dcs.model.Task;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class DcsApplicationTests {
-
-    @Before
-    public void setUp() {
-        Task task_one = new Task();
-    }
+@ExtendWith(SpringExtension.class)
+class DcsApplicationTests {
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
     }
 
 }
